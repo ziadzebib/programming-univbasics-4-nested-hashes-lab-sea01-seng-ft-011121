c1c45ebaa116ecbbbe70e9cef031620841bc1b79
Ziad Zebib <zebibziad@gmail.com>
2020-12-28 21:26:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,10 @@ def adding_matz
         :languages => ["C"]
       }
     }
-  programmer_hash[yukihiro_matsumoto]
+  programmer_hash[yukihiro_matsumoto] = {
+    :known_for "ruby"
+    :languages ["LISP", "C"]
+  }
 end
 
 def changing_alan
