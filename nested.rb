994899d5b5d0b928cab418880b0b1a1b4b0f6f2f
Ziad Zebib <zebibziad@gmail.com>
2020-12-28 21:34:04 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,8 @@ def hopper
         :languages => ["C"]
       }
     }
-
+  
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +56,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -81,7 +82,12 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:yukihiro_matsumoto] = {
+    known_for: "ruby",
+    languages: ["LISP", "C"]
+  }
+  
+  programmer_hash
 end
 
 def changing_alan
@@ -103,7 +109,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +133,9 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    
+  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+  
+  programmer_hash
 
 end
